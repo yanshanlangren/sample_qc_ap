175d89e05ea180186a2f5420838d1863818b39b5
yanshanlangren <elviszhai@yunify.com>
2020-11-26 11:02:36 +0800

    saas application

--- mysite/sample/control.py
+++ mysite/sample/control.py
@@ -1,92 +1,277 @@
-from django.conf import settings
-# get qingcloud python sdk from https://github.com/yunify/qingcloud-sdk-python
-from qingcloud.app.connection import AppConnection
-from qingcloud.conn.auth import AppSignatureAuthHandler
+from hashlib import sha256
+import time
+import rfc822
+import base64
+import hmac
+import urllib
+import requests
 
-class AuthException(Exception):
-    pass
+app_id = "app-cvts1w3s"
+
+BASE_SSO_URL = "http://sso.nscccloud.com"
+BASE_API_URL = "http://api.nscccloud.com:7777"
+
 
 class QcControl:
 
-    def __init__(self, payload, signature):
-        auth = AppSignatureAuthHandler(settings.APP_ID,
-                                       settings.SECRET_APP_KEY)
+    def __init__(self, access_token, refresh_token):
+        self.access_key = ""
+        self.secret_key = ""
+        self.token = ""
+        self.user = ""
+        self.access_token = access_token
+        self.refresh_token = refresh_token
 
-        self.req = auth.extract_payload(payload, signature)
-        if not self.req:
-            raise AuthException("Incorrect signature")
+        check_token_resp = self.check_token()
+        if check_token_resp is None:
+            print("check access_token failed")
+            return
 
-        self.zone = self.req["zone"]
-        self.conn = self.get_connection(self.req.get("access_token"),
-                                        self.zone)
-        self.app_action = self.req["action"]
-        self.is_notification = True
         self.handle_map = {
-                          "view_app": self.view_app,
-                          "install_app": self.install_app,
-                          "uninstall_app": self.uninstall_app,
-                          "suspend_resource": self.suspend_resource,
-                          "resume_resource": self.resume_resource,
-                          "terminate_resource": self.terminate_resource
-                          }
+            "DescribeUserInfo": self.handle_describe_user_info,
+            "NBDescribeBills": self.handle_nb_describe_bills,
+            "NBGetCostsSimple": self.handle_get_costs_simple,
+            "NBGetPrice": self.handle_nb_get_price,
+            "NBCreatePrdOrderSimple": self.handle_nb_create_prd_order_simple,
+            "NBSearchPrdOrders": self.handle_nb_search_prd_orders,
+            "NBChargePrdOrder": self.handle_nb_charge_prd_order,
+            "NBCancelPrdOrder": self.handle_nb_cancel_prd_order,
+            "NBDescribeProdInstances": self.handle_nb_describe_prod_instances,
+            "NBPauseProdInstance": self.handle_nb_pause_prod_instance,
+            "NBRenewProdInstance": self.handle_nb_renew_prod_instance,
+            "NBResumeProdInstance": self.handle_nb_resume_prod_instance,
+            "NBStopProdInstance": self.handle_nb_stop_prod_instance,
+        }
 
-    def handle(self):
-        print "handle action of %s"%(self.app_action)
-        self.handle_map[self.app_action]()
-        
+    def handle(self, action, **params):
+        print "handle action of %s" % action
+        return self.handle_map[action](action, **params)
+
+    def handle_describe_user_info(self, user_id, **params):
+        if isinstance(user_id, list):
+            user_id = user_id[0]
+        req = {
+            "action": "DescribeUsers",
+            "user_id": user_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_get_costs_simple(self, **params):
+        req = {
+            "action": "NBGetCostsSimple",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_get_price(self, **params):
+        req = {
+            "action": "NBGetPrice",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_create_prd_order_simple(self, **params):
+        req = {
+            "action": "NBCreatePrdOrderSimple",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_search_prd_orders(self, **params):
+        req = {
+            "action": "NBSearchPrdOrders",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_describe_bills(self, **params):
+        req = {
+            "action": "NBDescribeBills",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_charge_prd_order(self, **params):
+        req = {
+            "action": "NBChargePrdOrder",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_cancel_prd_order(self, **params):
+        req = {
+            "action": "NBCancelPrdOrder",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_describe_prod_instances(self, **params):
+        req = {
+            "action": "NBDescribeProdInstances",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_pause_prod_instance(self, **params):
+        req = {
+            "action": "NBPauseProdInstance",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_renew_prod_instance(self, **params):
+        req = {
+            "action": "NBRenewProdInstance",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_resume_prod_instance(self, **params):
+        req = {
+            "action": "NBResumeProdInstance",
+            "app_id": app_id,
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
+    def handle_nb_stop_prod_instance(self, **params):
+        req = {
+            "action": "NBStopProdInstance",
+            "app_id": app_id,
+            "search_word": "xxxx",
+        }
+        resp = self.send_appcenter_request(req)
+        return resp
+
     def get_user_info(self):
-        user_set = self.conn.describe_users()
-        if user_set:
-            return user_set['user_set'][0]
-        return None
+        return self.user
 
-    def get_eips(self):
-        return self.conn.describe_eips(status=["available", 
-                                               "associated",
-                                               "suspended"])
+    def check_token(self):
+        url = BASE_SSO_URL + "/sso/check_token/"
+        resp = requests.post(
+            url=url,
+            headers={
+                "Authorization": "Bearer " + self.access_token,
+                # 'content-type': 'application/x-www-form-urlencoded',
+                'date': rfc822.formatdate(time.time())
+            })
+        if resp.status_code != 200:
+            return
+        check_token_resp = resp.json()
+        print("check token response[%s]" % check_token_resp)
+        self.access_key = get_utf8_value(check_token_resp.get("access_key"))
+        self.secret_key = get_utf8_value(check_token_resp.get("secret_key"))
+        self.token = get_utf8_value(check_token_resp.get("token"))
+        self.user = check_token_resp.get("user")
+        return "success"
 
-    def get_connection(self, access_token, zone):
-        return AppConnection(settings.APP_ID,
-                             settings.SECRET_APP_KEY,
-                             zone,
-                             access_token=access_token)
+    def refresh_token(self):
+        url = BASE_SSO_URL + "/sso/refresh_token/"
+        resp = requests.post(
+            url=url,
+            headers={
+                "Authorization": "Bearer " + self.access_token,
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'date': rfc822.formatdate(time.time())
+            },
+            data={
+                "grant_type": "refresh_token",
+                "refresh_token": self.refresh_token
+            }
+        )
+        if resp.status_code != 200:
+            return
+        refresh_token_resp = resp.json()
+        self.access_token = refresh_token_resp.get("access_token")
+        self.refresh_token = refresh_token_resp.get("refresh_token")
+        return "success"
 
-    def view_app(self):
-        self.is_notification = False
+    def send_appcenter_request(self, req):
+        url = BASE_API_URL + "/iam/"
+        params = {
+            'access_key_id': self.access_key,
+            'open_name': 'appcenter',
+            'signature_version': 1,
+            'signature_method': 'HmacSHA256',
+            'time_stamp': get_ts(),
+            'token': self.token,
+        }
+        params.update(**req)
+        sign, query_string = get_signature(params, self.secret_key, "/iam/", method="GET")
+        url += '?%s&signature=%s&token=%s' % (query_string, sign, self.token)
+        print("url[%s]" % url)
+        resp = requests.get(url=url, verify=False)
 
-    def install_app(self):
-        # you can inital user management in this event
-        # or leave it to view_app
+        if resp.status_code == 401:
+            if self.refresh_token() is None:
+                print("failed to refresh token")
+                return
+            if self.check_token() is None:
+                print("failed to check token")
+                return
+        print("IAM response:[%s]" % resp.json())
+        return resp.json()
 
-        print "%s installed App"%self.req["user_id"]
 
-    def uninstall_app(self):
-        # stop all your service and unlease-app for this user
+ISO8601 = '%Y-%m-%dT%H:%M:%SZ'
 
-        print "%s uninstalled App"%self.req["user_id"]
 
-        user_id    = self.req["user_id"]
-        zone       = self.req["zone"]
-        self.get_connection(None, zone).unlease_app([user_id, settings.APP_ID])
+def get_ts(ts=None):
+    ''' get formatted UTC time '''
+    if not ts:
+        ts = time.gmtime()
+    return time.strftime(ISO8601, ts)
 
-    def suspend_resource(self):
-        # do something to stop your service
 
-        print "App resource %s for %s of %s has been suspended" \
-                %(self.req["appr_id"], self.req["service_id"],
-                  self.req["user_id"])
+def get_signature(params, secret_access_key, path, method="POST"):
+    string_to_sign = '%s\n%s\n' % (method, path)
+    keys = sorted(params.keys())
+    pairs = []
+    for key in keys:
+        val = get_utf8_value(params[key])
+        pairs.append(urllib.quote(key, safe='') + '=' + urllib.quote(val, safe='-_~'))
+    qs = '&'.join(pairs)
+    string_to_sign += qs
+    print("string_to_sign[%s]" % string_to_sign)
+    h = hmac.new(secret_access_key, digestmod=sha256)
+    h.update(string_to_sign)
+    sign = base64.b64encode(h.digest()).strip()
+    signature = urllib.quote_plus(sign)
 
-    def resume_resource(self):
-        # do something to resume your service
+    return signature, qs
 
-        print "App resource %s for %s of %s has been resumed" \
-                %(self.req["appr_id"], self.req["service_id"],
-                  self.req["user_id"])    
 
-    def terminate_resource(self):
-        print "Related QC resource of %s for %s of %s has been terminated" \
-                %(self.req["appr_id"], self.req["service_id"],
-                  self.req["user_id"])
-        appr_id    = self.req["appr_id"]
-        zone       = self.req["zone"]
-        self.get_connection(None, zone).unlease_app([appr_id])
+def get_utf8_value(value):
+    if not isinstance(value, str) and not isinstance(value, unicode):
+        value = str(value)
+    if isinstance(value, unicode):
+        return value.encode('utf-8')
+    else:
+        return value
+
+
+if __name__ == '__main__':
+    req = {
+        'action': 'DescribeUsers',
+        'access_key_id': 'W3wyXVT05ji70pscFQEkXQ',
+        'open_name': 'appcenter',
+        'signature_version': 1,
+        'signature_method': 'HmacSHA256',
+        'time_stamp': '2020-11-25T07:48:07Z',
+        'user_id': 'admin',
+        'token': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY3IiOiIxIiwiYXVkIjoiaWFtIiwiYXpwIjoiaWFtIiwiY29ucyI6Im5zY2NjbG91ZCIsImN1aWQiOiJpYW1yLXBlN2lpamxuIiwiZWlzayI6IjRYR05vMS1HenNZSUpvcmRBTXR6STJoZmVOdjMwU1pNNTJBQlVNMW1fTG89IiwiZXhwIjoxNjA2MjkzMjczLCJpYXQiOjE2MDYyODk2NzMsImlzcyI6InN0cyIsImp0aSI6Ilczd3lYVlQwNWppNzBwc2NGUUVrWFEiLCJuYmYiOjAsIm9yZ2kiOiJhcHAtQ1FIWXJhS2YiLCJvd3VyIjoiYWRtaW4iLCJwcmVmIjoicXJuOnFpbmdjbG91ZDppYW06Iiwicm91ciI6ImFkbWluIiwicnR5cCI6InJvbGUiLCJzdWIiOiJzdHMiLCJ0eXAiOiJJRCJ9.Lf-zIRcYDf5t9Qqqki3Q2f1xAh5sWd_kaSm1_JtS-m9SXG7FaCgXsI3ha_VdL88SUeAn16w_b8--MQX0d00aclZIiSw8GLwEb6q2tgH0GCP5WAYD9BRu1outcqKspKieSzwx3uH7I462i9YHMpcwuF4lRlEiSVPwtrnPXypd1rg',
+    }
+    sign, query_string = get_signature(req, 'qUbMeccvYb8a4c0', "/`/", method="GET")
+    print("signature[%s]" % sign)
+    print("url[https://api.qingcloud.com/iaas/?%s&signature=%s]" % (query_string, sign))
--- mysite/sample/views.py
+++ mysite/sample/views.py
@@ -1,12 +1,13 @@
-from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from sample.control import QcControl
 import traceback
 from django.views.decorators.clickjacking import xframe_options_exempt
 from django.template import RequestContext, loader
+import json
 
-user_control= {}
+user_control = {}
+
 
 # allow post from other site
 @csrf_exempt
@@ -15,88 +16,53 @@ user_control= {}
 def index(request):
     try:
         qc = get_qc(request)
-        qc.handle()
-        if qc.is_notification:
-            return HttpResponse("OK")
-        else:
-            user = qc.get_user_info()
-            user_id = user["user_id"]
-            zone = qc.zone
-    
-            # in real case, you may need to manage user and tokens in database
-            # in this example, store them in object for simply
-            if user_id not in user_control:
-                user_control[user_id] = {}
-            if zone not in user_control[user_id]:
-                user_control[user_id][zone] = {}
-            user_control[user_id][zone]["qc"] = qc
-            
-            # disable service status for each eip
-            eips = qc.get_eips()["eip_set"]
-            for eip in eips:
-                eip_id = eip["eip_id"]
-                if eip["eip_id"] in user_control[user_id][zone] and \
-                        user_control[user_id][zone][eip_id]:
-                    eip["btn_name"] = "disable"
-                else:
-                    eip["btn_name"] = "enable"
-                
-            template = loader.get_template('index.html')
-            context = RequestContext(request, {
-                'username'    : user['user_name'],
-                'user_id'     : user_id,
-                'zone'        : zone,
-                'notify_email': user['notify_email'],
-                'eips'        : eips
-            })
-            return HttpResponse(template.render(context))
-    except Exception:
+        if not qc:
+            return HttpResponse("not autherized")
+        user = qc.get_user_info()
+        if not user:
+            return HttpResponse("not autherized")
+        user_control[user["user_id"]] = qc
+        template = loader.get_template('index.html')
+        context = RequestContext(
+            request,
+            {
+                'username': user['user_name'],
+                'user_id': user['user_id'],
+                'notify_email': user['email'],
+            }
+        )
+        return HttpResponse(template.render(context))
+    except Exception as e:
+        print(e.message)
         exstr = traceback.format_exc()
-        print exstr
+        print(exstr)
         return HttpResponse(exstr)
+
 
 # allow post from other site
 @csrf_exempt
 def service(request):
     try:
-        qc_resource_id = request.POST.get("resource_id")
-        appr_id        = request.POST.get("appr_id")
-        user_id        = request.POST["user_id"]
-        zone           = request.POST["zone"]
-        action         = request.POST["action"]
-        qc             = user_control[user_id][zone]["qc"]
-        if action == "enable":
-            # you will need to provide service to your user here
-            
-            rep = qc.conn.lease_app(settings.SERVICE_ID, qc_resource_id)
-            print rep
-            if rep:
-                appr_id = rep["appr_id"]
-                # store appr_id to unlease it later
-                user_control[user_id][zone][qc_resource_id] = appr_id
-                print "store appr_id %s to qc id %s"%(appr_id, qc_resource_id)
-        elif action == "disable":
-            # stop your service and then unlease app
-    
-            # unlease app does not need access_token
-            appr_id = user_control[user_id][zone][qc_resource_id]
-            print "unlease appr [%s]"%(appr_id)
-            if appr_id:
-                rep = qc.get_connection(None, zone).unlease_app([appr_id])
-                if rep:
-                    del user_control[user_id][zone][qc_resource_id]
-                print "unlease appr [%s] result is [%s]"%(appr_id, rep)
+        user_id = request.POST["user_id"]
+        action = request.POST["action"]
+        params = request.POST
+        qc = user_control[user_id]
+        if action in qc.handle_map:
+            response = qc.handle_map[action](**params)
+            return HttpResponse(json.dumps(response))
         else:
-            return HttpResponse("not supported action %s"%action)
+            return HttpResponse("not supported action %s" % action)
 
     except Exception:
         exstr = traceback.format_exc()
         print exstr
         return HttpResponse(exstr)
-    return HttpResponse("ok")
+
 
 def get_qc(request):
-    payload   = request.POST["payload"]
-    signature = request.POST["signature"]
-
-    return QcControl(payload, signature)
+    if "access_token" not in request.GET or "refresh_token" not in request.GET:
+        print("not authorized")
+        return None
+    access_token = request.GET["access_token"]
+    refresh_token = request.GET["refresh_token"]
+    return QcControl(access_token, refresh_token)
